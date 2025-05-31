2d7f89b3d72742cfb5da4c6e432a37dda96ac803
Dan <1334054+Miff-Real@users.noreply.github.com>
2025-05-31 12:45:20 +0100

    Do not use static inline
    To be compatible with C++11
    
    Move the static variables outside of structs

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <cmath>  // For std::acos
 #include <rack.hpp>
 
 extern rack::plugin::Plugin *pluginInstance;
 
 extern rack::plugin::Model *modelDanTTemplate;
+
+namespace DANT {
+static const double PI = std::acos(-1.0);
+}

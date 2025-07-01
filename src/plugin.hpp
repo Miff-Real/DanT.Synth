f32a59a68f030e5f6526bb56efc64a6b0c51451e
Dan <1334054+Miff-Real@users.noreply.github.com>
2025-07-01 15:56:25 +0100

    Add some consts for working with simd

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -16,6 +16,11 @@ static const float _Y = rack::app::RACK_GRID_HEIGHT / 32.0f;
 
 namespace DANT {
 static const double PI = std::acos(-1.0);
+static const int CHANS{16};                                                      // used for array sizing
+static const int SIMD{4};                                                        // used for simd looping
+static const int SIMD_I[CHANS]{0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};  // float[16] index to float_4[4] index
+static const int SIMD_J[CHANS]{0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};  // channel index to float_4 index
+static const rack::simd::float_4 SIMD_ZERO{0.0f, 0.0f, 0.0f, 0.0f};
 
 static rack::math::Vec layout(const float column, const float row) {
   return rack::math::Vec(_X * ((column * 2.0f) - 1.0f), _Y * ((row * 2.0f) - 1.0f));

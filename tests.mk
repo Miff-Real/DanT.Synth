5d8ddda6827f1156f4fd4606e4a92f7bce9ae4a5
Steve Russell <steve.russell33@gmail.com>
2025-08-12 11:42:33 +0100

    Fix for tests on Windows

--- tests.mk
+++ tests.mk
@@ -12,9 +12,10 @@ ifeq ($(ARCH_OS), mac)
 	TEST_INCLUDE_DIRS = -Isrc/dsp -I$(CATCH2_DIR) -I$(RACK_DIR) -I$(RACK_DIR)/include -I$(RACK_DIR)/dep/include
 	TEST_LDFLAGS += -L$(RACK_DIR) -lRack -Wl,-rpath,$(RACK_DIR) $(ARCH_FLAG)
 else
-	TEST_INCLUDE_DIRS = -L/mingw64/lib -Isrc/dsp -I$(CATCH2_DIR) -I$(RACK_DIR) -I$(RACK_DIR)/include -I$(RACK_DIR)/dep/include
+	RACK_APP_DIR = "/c/Program Files/VCV/Rack2 Pro/"
+#	TEST_INCLUDE_DIRS = -L/mingw64/lib -Isrc/dsp -I$(CATCH2_DIR) -I$(RACK_DIR) -I$(RACK_DIR)/include -I$(RACK_DIR)/dep/include -I$(RACK_APP_DIR)
+	TEST_INCLUDE_DIRS = -Isrc/dsp -I$(CATCH2_DIR) -I$(RACK_DIR) -I$(RACK_DIR)/include -I$(RACK_DIR)/dep/include -I$(RACK_APP_DIR)
 	TEST_LDFLAGS += -L$(RACK_DIR) -lRack -lnanovg
-	RACK_APP_DIR = "/c/Program Files/VCV/Rack2Pro"
 endif
 
 .PHONY: test clean_tests
